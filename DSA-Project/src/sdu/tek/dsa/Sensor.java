ccea6b32ba4c106e9a16a3fdc5892d1a7b71499b
Stefan <S_Spasov@S_Spasov-PC>
2012-03-09 12:58:11 +0100

    1

--- DSA-Project/src/sdu/tek/dsa/Sensor.java
+++ DSA-Project/src/sdu/tek/dsa/Sensor.java
@@ -1,5 +1,12 @@
 package sdu.tek.dsa;
 
 public interface Sensor {
-	//something 2
-}
+	  // return an unique identifier for this object
+    public int getSensorId();
+
+    // return a value between -20.00°C and 35.00°C
+    public String getTemperature();
+    
+    // return a value between 0.0% and 100.0%
+    public String getHumidity();
+    }
